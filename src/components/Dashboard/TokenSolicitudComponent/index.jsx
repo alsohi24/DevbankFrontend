9f1edb7b4be4d8379995e7712ded886653433818
Alvaro Soto <alsohi24@gmail.com>
2020-12-02 23:32:53 -0500

    Se agregan popups de validacion y generascion de codigo token

--- src/components/Dashboard/TokenSolicitudComponent/index.jsx
+++ src/components/Dashboard/TokenSolicitudComponent/index.jsx
@@ -10,27 +10,48 @@ const {useState, useEffect} = React;
 export default (props) =>{
     const [showModal, setShowModal] = useState(false);
     const [showModalError, setShowModalError] = useState(false);
+    const [showSuccess, setShowSuccess] = useState(false);
 
+
+    const [codigo, setCodigo] = useState(false);
+    const [form, setForm] = useState(false);
 
     function crear(){
         setShowModal(true);
+    }
+    function handleForm(event) {
+        let inputName = event.target.name;
+        let inputValue = event.target.value;
+        setForm({...form, [inputName]: inputValue})
     }
 
     function crearSolictitud(){
         //SESSION
         const body = {
             "soliEstado":"1",
-            "soliUsu":"strngrwrld",
+            "soliUsuId":"1",
             "soliOperacion":"CREAR"
         }
-        SolicitudesController.postProcesarSolicitud(body).then(({data}) => {
-            const result = data.data;
-            console.log(data)
-        }).catch((error) => {
-            console.log('Error', error);
-            //setShowModal(false);
-            setShowModalError(true);
-        });
+        // SolicitudesController.postProcesarSolicitud(body).then(({data}) => {
+        //     const result = data.data;
+        //     console.log(data)
+        // }).catch((error) => {
+        //     console.log('Error', error);
+        //     //setShowModal(false);
+        //     setShowModalError(true);
+        // });
+        setCodigo(true);
+    }
+    function validarCodigo(){
+        //SESSION
+        const body = {
+            "soliEstado":"1",
+            "soliUsuId":"1",
+            "soliOperacion":"CREAR"
+        }
+        setShowModal(false)
+        setShowSuccess(true)
+        //setCodigo(true);
     }
 
     return (
@@ -42,7 +63,18 @@ export default (props) =>{
             >         
                 <div>
                 <h3 className="e-h3" >Active su token para poder realizar movimientos!</h3>
-                <button onClick={()=>{crearSolictitud()}} className="btn_atc c_token__button e-p3 e-p6:lg" >Activar</button>
+
+                    {codigo &&
+                    <>
+                        <input onChange={handleForm}  name="codigo" className="btn_atc  c_token__field_input e-p2 e-p4:md" ></input>
+                        <button onClick={()=>{validarCodigo()}} className="btn_atc c_token__button e-p3 e-p6:lg" >Enviar código</button>
+                        </>
+                    }
+                {!codigo &&
+                    <>
+                        <button onClick={()=>{crearSolictitud()}} className="btn_atc c_token__button e-p3 e-p6:lg" >Activar</button>
+                   </>
+                    }
                 </div>
             </ModalComponent>
                 <h3 className="e-p1"></h3>
@@ -58,6 +90,18 @@ export default (props) =>{
                 }
             >
                 <h3 className="e-h3">Intente de nuevo</h3>
+            </ModalComponent>
+            <ModalComponent
+                title=""
+                show={showSuccess}
+                handleClose={()=>{setShowSuccess(false);}}
+                footer={
+                    <div className="p_cuentas__modal_buttons">
+                        <ButtonComponent theme="primary" action={()=>{setShowSuccess(false);}}>Cerrar</ButtonComponent>
+                    </div>
+                }
+            >
+                <h3 className="e-h3">Succes!</h3>
             </ModalComponent>
         </div>
     )
